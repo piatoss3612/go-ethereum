da271719d252ca5e051357390da40557e5f09650
piatoss <crewe1746@naver.com>
2024-01-22 19:28:27 +0900

    Add comments on common/math package

--- common/bytes.go
+++ common/bytes.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package common contains various helper functions.
+// common 패키지는 여러 가지 도우미 함수를 포함합니다.
 package common
 
 import (
